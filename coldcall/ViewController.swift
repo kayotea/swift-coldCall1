5c271c9f0683e25b5c56b2de51632a47067b31b0
Placoderm <placoderm@Sarah-MacBook-Air.local>
2017-07-05 20:01:00 -0700

    initial commit

--- coldcall/ViewController.swift
+++ coldcall/ViewController.swift
@@ -10,6 +10,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let names = [
+        "Bryant",
+        "Courtney",
+        "Jay",
+        "Ryota",
+        "Uyanga"
+    ]
+    var name_idx = 0
+    
+    @IBOutlet weak var contactName: UILabel!
+    @IBAction func callButton(_ sender: UIButton) {
+        name_idx = Int(arc4random_uniform(5))
+        updateUI()
+    }
+    
+    func updateUI() {
+        contactName.text = self.names[name_idx]
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
